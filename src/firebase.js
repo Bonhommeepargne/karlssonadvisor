2cc1610e509f784f493c922387b2e4182dd735a9
Bonhommeepargne <37111058+Bonhommeepargne@users.noreply.github.com>
2021-02-13 17:57:11 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const config = {
-    apiKey: "AIzaSyB6EaUyOgZovgkhQNtavUknKmwonKTFypc",
-    authDomain: "karlsson-b78fc.firebaseapp.com",
-    databaseURL: "https://karlsson-b78fc.firebaseio.com/",
-    projectId: "karlsson-b78fc",
-    storageBucket: "karlsson-b78fc.appspot.com",
-    messagingSenderId: "305544346220",
-    appId: "1:305544346220:web:0692e885a3441149d77649",
-    measurementId: "G-VPK27MT23L"
+
 };
 
 export async function login(email, password) {
